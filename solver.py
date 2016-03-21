d6fa1b3be82a00d021aa5326352ae26f4d2a8478
Jake Wright <admin@jacobwright.com>
2016-03-20 23:07:10 -0700

    Added additional methods for screening impossible values.  Need to add deductive logic to solve for values still.  Want to play around with a 'guess' mode for underconstrained boards as well.

--- solver.py
+++ solver.py
@@ -16,11 +16,37 @@ class Board(object):
         In the future make the default have no board data, but give the option to generate a random board,
         generate from a string (123...48......2.), you get the idea.
         """
-        if type(grid)==str:
-            grid=np.load(grid)['board']
         self.__board=np.zeros((9,9),np.uint8)
         self.__poss=np.ones((9,9,9),np.bool) #use 3D array to hold possible numbers 
         self.__batchUpdate(grid)
+
+    @classmethod
+    def load(cls, filename):
+        """Method to create board from previously saved boards."""
+        grid=np.load(filename)['board']
+        return cls(grid)
+
+    @classmethod
+    def fromMaskedStrings(cls, solution, mask):
+        """Method to create puzzle from a pair of strings, one for solution and one for mask."""
+        assert len(solution)==len(mask), 'Length of solution and mask are not the same!'
+        output=''
+        for i in range(len(solution)):
+            if mask[i]=='1': output+='.'
+            else: output+=solution[i]
+        return cls.fromString(output)           
+
+    @classmethod
+    def fromString(cls, string):
+        """
+        Creates board from 81 character string using . to signify unsolved boxes
+        """
+        board=np.zeros((9,9),np.uint8)
+        for row in range(9):
+            for col in range(9):
+                if not string[row*9+col]=='.':
+                    board[row,col]=int(string[row*9+col])
+        return cls(board)
 
     def saveBoard(self,fname):
         np.savez(fname,board=self.__board)
@@ -38,6 +64,15 @@ class Board(object):
     def __checkPossible(self,location):
         row, col = location
         return np.nonzero(self.__poss[:,row,col])[0]
+
+    def __checkPossibleCell(self,grid):
+        y,x=np.nonzero(grid)
+        locations=zip(y,x)
+        return locations
+
+    def __checkPossibleRC(self,grid):
+        return np.nonzero(grid)[0]
+
 
     def updateBoard(self, location, value):
         """
@@ -76,6 +111,107 @@ class Board(object):
     def getBoard(self):
         return np.array(self.__board,np.uint8)
 
+    def __pairExclusion(self):
+        """Method to reduce possibilities by induction.  Expensive, so called only when needed.
+        This catches the standard 'swordfish' and 'xwing' solving techniques.
+        I'm retiring this in favor of simple exclusion as it catches all cases.
+        This is easier for people to find, __simpleExclusion is more efficient.  And shorter.
+        """
+        
+        _='' #keep indentation in correct place.
+        #do first by rows.
+        #look for rows with two and only two possibilities for a given number.
+        for plane in range(9):
+            roi={} #rows of interest.  get it? like region of interest?
+            for row in range(9):
+                possible=self.__checkPossibleRC(self.__poss[plane,row,:])
+                if len(possible)==2:
+                    roi[row]=possible #store the columns for later.
+            #now we have to actually do something with that data...
+            #for each value in each row, check to see if they have anything in common with
+            #another value from another row.  
+            #this is guaranteed to be 2x2, implement a checkCommon function for this?
+            for key in roi.keys():
+                p1 = (key,roi[key][0])
+                p2 = (key,roi[key][1])
+                for key2 in roi.keys():
+                    if not key==key2:
+                        p3=(key2,roi[key2][0])
+                        p4=(key2,roi[key2][1])
+                        self.__checkCommon([p1,p2],[p3,p4],plane)
+                        #TODO: Implement checkCommon.  This should compare the point sets and tell me
+                        #if there is any commonality between the two sets and if so, what.
+                        #e.g. p1,p3 share column 0, p2,p4 share cell 3.  I guess best case scenario
+                        #would be a double share (p2,p4 share cell 3, column 8) then we can reduce the possibility
+                        #from every other spot in cell3 and every other spot in column 8.
+
+    def __simpleExclusion(self):
+        """Simpler form of exclusion testing.  Reduces possibilities for a given number set."""
+        #Basic idea: look at cells to see possibilities for a given number.  If they fall in the same row/col
+        #Then elemenate the number from possibility for the rest of the rows/cols.
+        
+        #Second check: if row or col requires a number and all possibilities are in a given cell,
+        #remove that possibility from the rest of the cell. (write 0 to whole cell, add back in 1's where 
+        #the row/col possibilities were.
+
+        #Noticing a trend here... everything has to be thought of for cell, row, and column.
+        reduced=False
+        for plane in range(0,9):
+            for row in range(0,9,3):
+                for col in range(0,9,3):
+                    y,x=np.nonzero(self.__poss[plane,row:row+3,col:col+3])
+                    if len(x)>0:
+                        if np.all(x==x[0]):
+                            if not len(np.nonzero(self.__poss[plane,:,col+x[0]])[0]) == len(x):
+                                #if we made it here everything was in the same column.
+                                #set all other states to 0 (can't happen)
+                                self.__poss[plane,:,col+x[0]]=False
+                                self.__poss[plane,row:row+3,col:col+3][y,x]=True #and reset our remaining values
+                                reduced=True
+                        if np.all(y==y[0]):
+                            if not len(np.nonzero(self.__poss[plane,row+y[0],:])[0]) == len(x):
+                                #if we made it here everything was in the same row.
+                                #set all other states to 0 (can't happen)
+                                self.__poss[plane,row+y[0],:]=False
+                                self.__poss[plane,row:row+3,col:col+3][y,x]=True #and reset our remaining values
+                                reduced=True
+        #TODO: repeat same check for row start (propogates to cols and cells)
+        #e.g. row needs value, all possible locations are in one cell, remove other possibilities in that cell
+        for plane in range(0,9):
+            for row in range(9):
+                x=np.nonzero(self.__poss[plane,row,:])[0]
+                if len(x)>2:
+                    if np.all(x<3) or np.all(x>5) or (np.all(x>3) and np.all(x<6)):
+                        #now we convert to cell notation
+                        cellx=int(x[0]/3)*3
+                        celly=int(row/3)*3
+                        if len(np.nonzero(self.__poss[plane,celly:celly+3,cellx:cellx+3])[0]) > len(x):
+                            self.__poss[plane,celly:celly+3,cellx:cellx+3]=False
+                            self.__poss[plane,row,:][x]=True
+                            reduced=True
+
+        #TODO: repeat same check for col start (propogates to cells)
+        for plane in range(0,9):
+            for col in range(9):
+                y=np.nonzero(self.__poss[plane,:,col])[0]
+                if len(y)>2:
+                    if np.all(y<3) or np.all(y>5) or (np.all(y>2) and np.all(y<6)):
+                        #now we convert to cell notation
+                        cellx=int(col/3)*3
+                        celly=int(y[0]/3)*3
+                        if len(np.nonzero(self.__poss[plane,celly:celly+3,cellx:cellx+3])[0]) > len(y):
+                            self.__poss[plane,celly:celly+3,cellx:cellx+3]=False
+                            self.__poss[plane,:,col][y]=True
+                            reduced=True
+        #e.g. col needs value, all possible locations are in one cell, remove other possibilities in that cell
+        return reduced
+                            
+
+
+
+
+
+
     def simpleSolve(self):
         """Simplest solving method, brute force.  Returns True if solved successfully, false if not."""
         #this will always return false.  this is a terrible strategy.
@@ -88,13 +224,43 @@ class Board(object):
                     if len(locs)==1:
                         self.updateBoard((i,j),locs[0]+1)
                         updated=True
+
+            #Check by cell
+            for plane in range(9):
+                num=plane+1
+                for row in range(0,9,3):
+                    for col in range(0,9,3):
+                        if not num in self.__board[row:row+3,col:col+3]:
+                            possible=self.__checkPossibleCell(self.__poss[plane,row:row+3,col:col+3])
+                            if len(possible)==1:
+                                self.updateBoard((possible[0][0]+row,possible[0][1]+col),num)
+                                updated=True
+            
+            #Check by row 
+            for plane in range(9):
+                num=plane+1
+                for row in range(0,9):
+                    if not num in self.__board[row,:]:
+                        possible=self.__checkPossibleRC(self.__poss[plane,row,:])
+                        if len(possible)==1:
+                            self.updateBoard((row,possible[0]),num)
+                            updated=True
+
+            #Check by col
+            for plane in range(9):
+                num=plane+1
+                for col in range(0,9):
+                    if not num in self.__board[:,col]:
+                        possible=self.__checkPossibleRC(self.__poss[plane,:,col])
+                        if len(possible)==1:
+                            self.updateBoard((possible[0],col),num)
+                            updated=True
+
             if np.sum(self.__board)==405:
                 return True
             if not updated:
-                return False
-
-
-
+                if not self.__simpleExclusion():
+                    return False
 
 if __name__=='__main__':
     print 'this is example code to add to get repo.'
